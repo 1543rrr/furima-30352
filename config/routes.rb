bff7ed5bd3d446d80094002b61cf72e9cad3f006
1543rrr <73112533+1543rrr@users.noreply.github.com>
2021-01-18 11:49:04 +0900

    Basic認証を導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root "items#index"
+  resources :items do
+    resources :comments,  only: [:create, :destroy]
+    resources :favorites, only: [:create, :destroy]
+    collection do
+      get 'get_category_children', defaults: { fomat: 'json'}
+      get 'get_category_grandchildren', defaults: { fomat: 'json'}
+      get 'search'
+      get 'post_done'
+      get 'delete_done'
+      get 'detail_search'
+      get 'update_done' # これを追加
+    end
+  end
 end
