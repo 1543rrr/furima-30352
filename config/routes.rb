36ef7a21edd621ad843d9faeab050f058e553d15
1543rrr <73112533+1543rrr@users.noreply.github.com>
2021-01-19 13:06:30 +0900

    画像の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "items#index"
-  resources :items do
-    resources :comments,  only: [:create, :destroy]
-    resources :favorites, only: [:create, :destroy]
-    collection do
-      get 'get_category_children', defaults: { fomat: 'json'}
-      get 'get_category_grandchildren', defaults: { fomat: 'json'}
-      get 'search'
-      get 'post_done'
-      get 'delete_done'
-      get 'detail_search'
-      get 'update_done' # これを追加
-    end
-  end
+  resources :items 
 end
