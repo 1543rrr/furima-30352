ba6a0b41883faa763758d5a5ea3a8c80feebbb00
1543rrr <73112533+1543rrr@users.noreply.github.com>
2021-01-29 22:09:27 +0900

    ユーザー管理機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   has_many :orders
   
   validates :birth_day, presence: true
- 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
   
@@ -24,3 +23,4 @@ class User < ApplicationRecord
   
  
 end
+
